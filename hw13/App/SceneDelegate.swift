cd0c8e240ec62712d7f20a68dfcc626109b6ac57
gazizovdastan <gazizovdastan@gmail.com>
2023-07-10 09:22:27 +0500

    basic settings

---
+++ hw13/App/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  hw13
+//
+//  Created by Dastan on 10.07.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ hw13/App/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  hw13
+//
+//  Created by Dastan on 10.07.2023.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    // MARK: - State
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Lifecycle
+
+    // MARK: - UI
+    
+    // MARK: - Setup
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
+    // MARK: - Actions
+
+}
+
